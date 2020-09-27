81898597cc24db55e6afec61493f11589ed5d4ca
lefourbefromage <vincent.waldmann@gmail.com>
2020-09-27 11:58:21 +0200

    add babel and style for hero header

---
+++ statics/js/morph-svg.js
@@ -0,0 +1,3 @@
+
+var tl = gsap.timeline({repeat: -1, repeatDelay: 0, yoyo:true, defaults:{duration:8, ease:Power1.easeInOut}});
+tl.to("#anim-1", {morphSVG:"#anim-2"}).to("#anim-1", {morphSVG:"#anim-3"});
